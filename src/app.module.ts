b5ad49bbe5843ff82b92dd4656de848717b5c337
medamur <1996.popov@gmail.com>
2021-07-17 12:20:02 +0300

    add type orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { JobModule } from './job/job.module';
+import { JobModule } from './job/job.module'
+import { configService } from './config/config.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+configService
 
 @Module({
-	imports: [JobModule],
+	imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()), JobModule],
 	controllers: [AppController],
 	providers: [AppService]
 })
